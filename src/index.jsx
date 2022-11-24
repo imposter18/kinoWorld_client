0eb273b68207ed6b10e629a60369da190d3ed1bb
unknown <pantomm@yandex.ru>
2022-11-24 03:49:49 +0300

    first commit, setting webpack

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import "./index.scss";
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App.jsx";
+
+render(<App></App>, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const loader = require("sass-loader");
+
+module.exports = {
+	mode: "development",
+
+	entry: ["@babel/polyfill", "./src/index.jsx"],
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "[name].[hash].js",
+	},
+	devServer: {
+		port: 3000,
+	},
+	resolve: {
+		extensions: [".js", "jsx"],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({ template: "./src/index.html" }),
+		new CleanWebpackPlugin(),
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.s?css$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.(png|jpe?g|gif|webm|mp4|svg)$/,
+				use: ["file-loader"],
+			},
+			{
+				test: /\.m?jsx$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+		],
+	},
+};
